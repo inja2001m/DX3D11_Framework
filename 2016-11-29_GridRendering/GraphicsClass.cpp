ff54f87c463f617889969cd874d5a0297e84ea95
inja2001m <dopa_1@naver.com>
2016-12-02 00:43:41 +0900

    Create GraphicsClass

---
+++ 2016-11-29_GridRendering/GraphicsClass.cpp
@@ -0,0 +1,25 @@
+#include "GraphicsClass.h"
+
+GraphicsClass::GraphicsClass(){}
+GraphicsClass::GraphicsClass(const GraphicsClass &){}
+GraphicsClass::~GraphicsClass(){}
+
+bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
+{
+	return true;
+}
+
+void GraphicsClass::Shutdown()
+{
+	return;
+}
+
+bool GraphicsClass::Frame()
+{
+	return true;
+}
+
+bool GraphicsClass::Render()
+{
+	return true;
+}
